75474a5f0d5ccd3c0bfe6b3494d42a9fba1b41ca
TestGymBei <64074046+TestGymBei@users.noreply.github.com>
2022-07-10 10:27:42 +0100

    Add files via upload

---
+++ Uebung_Teil1.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <time.h>
+
+int main () {
+
+	char e;
+
+	scanf("Stoppuhr starten? (y/n) %c\n", &e);
+
+	switch(){
+	case 'y': printf("Vergangene Zeit: %", )
+			break;
+	case 'n': printf("dann eben nicht - auf Wiedersehen!")
+			break;
+	default: printf("Undefinierte Eingabe. Bitte nochmal versuchen!")
+			break;
+	}
+
+	printf("Stoppuhr gestartet \n");
+
+
+
+	printf("Hello World");
+	return 0;
+}
